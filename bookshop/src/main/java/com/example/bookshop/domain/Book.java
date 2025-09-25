b4b9941e81d66d3b819107b5903fbe1e698fff39
damienbarros <damien.barros@outlook.com>
2025-09-25 12:23:30 +0100

    Added getters and setters
    Added getters and setters

--- bookshop/src/main/java/com/example/bookshop/domain/Book.java
+++ bookshop/src/main/java/com/example/bookshop/domain/Book.java
@@ -24,6 +24,46 @@ public class Book {
   private String isbn;
   private LocalDate publishedDate;
 
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public LocalDate getPublishedDate() {
+        return publishedDate;
+    }
+
+    public void setPublishedDate(LocalDate publishedDate) {
+        this.publishedDate = publishedDate;
+    }
+
     public Long getId() {
         return id;
     }
--- bookshop/src/main/java/com/example/bookshop/domain/Client.java
+++ bookshop/src/main/java/com/example/bookshop/domain/Client.java
@@ -15,6 +15,30 @@ public class Client {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
     @NotBlank
     private String name;
 
